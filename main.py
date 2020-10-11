d75c7010bbcfa33bec7e09cb1703cd26ed164dcf
Tilak <davetilak003@gmail.com>
2020-10-11 22:49:08 +0530

    Added pyfiglet and some professional formalities

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import time
 import plyer
 from plyer import notification
-
+import pyfiglet
+ascii_banner = pyfiglet.figlet_format("Drink Water Notifier")
+print(ascii_banner)
+print("Developer Tilak dave")
+time.sleep(2)
+print("Starting Notification ystem...")
+time.sleep(1)
+print("Getting ready...")
+time.sleep(2)
 
 if __name__ == "__main__":
     while True:
